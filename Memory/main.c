ee2820d12ecb643e15ba5d1ccf4d8d2f2b03d541
STierProgrammer <150156387+STierProgrammer@users.noreply.github.com>
2024-08-31 01:07:23 +0400

    New

--- Memory/main.c
+++ Memory/main.c
@@ -5,27 +5,28 @@
 #define MEMORY_POOL 1024
 
 typedef struct MemoryBlock {
-    size_t size;
-    bool isFree;
-    struct MemoryBlock* next;
+    size_t size; 
+    bool isFree; 
+    struct MemoryBlock* next; 
 } MemoryBlock;
 
-static char MemoryAddressPool[MEMORY_POOL];
+static char MemoryAddressPool[MEMORY_POOL]; 
 
-static MemoryBlock* Memory = (MemoryBlock*)MemoryAddressPool;
+static MemoryBlock* Memory = (MemoryBlock*)MemoryAddressPool; 
 
 void InitializeMemory() {
     Memory->size = MEMORY_POOL + sizeof(MemoryBlock); 
-    Memory->isFree = true;
+    Memory->isFree = true; 
     Memory->next = NULL;
 }
 
-void divide(MemoryBlock* currentBlock, size_t size) {
+void divide(MemoryBlock* currentBlock, size_t size) 
+{
     size_t totalSize = size + sizeof(MemoryBlock);
 
-    if (currentBlock->size <= totalSize) {
-        currentBlock->isFree = false;
-        return;
+    if (currentBlock->size <= totalSize) 
+    {
+        currentBlock->isFree = false; return;
     }
 
     MemoryBlock* newBlock = (MemoryBlock*)((char*)currentBlock + totalSize);
@@ -40,7 +41,8 @@ void divide(MemoryBlock* currentBlock, size_t size) {
 }
 
 
-void* MallocRipoff(size_t size) {
+void* MallocRipoff(size_t size) 
+{
     MemoryBlock* currentBlock = Memory;
 
     while (currentBlock != NULL) {
@@ -56,47 +58,45 @@ void* MallocRipoff(size_t size) {
     return NULL;
 }
 
-void FreeRipoff(void* ptr) {
+void FreeRipoff(void* ptr) 
+{
     if (!ptr) return;
 
     MemoryBlock* currentBlock = (MemoryBlock*)((char*)ptr - sizeof(MemoryBlock));
 
     currentBlock->isFree = true;
 
-    if (currentBlock->next && currentBlock->next->isFree) {
+    if (currentBlock->next && currentBlock->next->isFree)
         currentBlock->size += currentBlock->next->size;
         currentBlock->next = currentBlock->next->next;
-    }
-    
+       
     MemoryBlock* block = Memory;
 
-    while (block && block->next != currentBlock) {
+    while (block && block->next != currentBlock) 
         block = block->next;
-    }
     
-    if (block && block->isFree) {
+    if (block && block->isFree) 
         block->size += currentBlock->size;
         block->next = currentBlock->next;
-    }
 }
 
 void PrintMemory() {
     MemoryBlock* currentBlock = Memory;
 
-    while (currentBlock != NULL) {
+    while (currentBlock != NULL) 
         printf("Address: %p, Size: %zu\n", (void*)currentBlock, currentBlock->size);
-        
+
         currentBlock = currentBlock->next;
-    }
 }
 
 int main() {
     InitializeMemory();
 
-    int* ptr1 = (int*)MallocRipoff(24);
+    int* ptr1 = (int*)MallocRipoff(sizeof(int));
+    
     *ptr1 = 4;
 
-    char* ptr2 = (char*)MallocRipoff(4);
+    char* ptr2 = (char*)MallocRipoff(sizeof(char));
 
     PrintMemory();
 
